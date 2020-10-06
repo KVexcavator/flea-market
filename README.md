1749b092d554a4d5d0d1a9466a001c09668fa744
KVexcavator <str_token@mail.ru>
2020-10-06 16:35:38 +0300

    refresh success impl

--- README.md
+++ README.md
@@ -1,6 +1,15 @@
+## Version
+- ruby-2.7.0 Ubuntu 18.04
+## Prepare project
 - bundle
 - sudo systemctl status mongodb
 - sudo systemctl start mongodb
 - ruby app.rb -e development/production/test
+## Apply 
 - bundle exec ruby app.rb
 - localhost:3000
+## httpie
+# Inspect setting
+- http :3000/
+# Clean the database and create the initial data
+- http :3000/refresh
--- app.rb
+++ app.rb
@@ -7,7 +7,7 @@ Mongoid.load!(File.join(File.dirname(__FILE__), 'config', 'mongoid.yml'))
 
 # Configuration
 configure :development do
-  enable :sessions
+  #enable :sessions
   # settings.name
   set :port, 3000
   set :max_lots, 10
@@ -17,10 +17,12 @@ end
 # Models
 class User
   include Mongoid::Document 
+  field :id, type: String  
+  field :_id, type: String, default: ->{ id }
   field :name, type: String
   field :purse, type: Float 
-  embeds_many :gismos, dependent: :delete_all
-  has_many :lots, foreign_key: 'user_ref', primary_key: 'name'
+  embeds_many :gismos
+  has_many :lots, foreign_key: 'seller', primary_key: 'name'
 
   # get '/login/:id'
   # session[:user_id] = params[:id]
@@ -32,26 +34,35 @@ end
 
 class Gismo
   include Mongoid::Document
-  field :name, type: String 
-  field :discription, type: String
+  field :name, type: String
+  field :_id, type: String, default: ->{ name }
+  field :quantity, type: Integer 
   field :price, type: Float 
   embedded_in :user 
 end
 
 class Lot
   include Mongoid::Document
-  field :user_ref, type: String 
-  field :name, type: String 
-  field :quantity, type: Integer
-  field :total, type: Float 
+  include Mongoid::Timestamps::Updated
+  field :seller, type: String 
+  # dynamically generated
+  field :description, type: String 
   # if true user.purse - settings.advertising_fee
   field :advertised, type: Boolean, default: false 
   # user = User.find(id)
   # user.lots 
-  belongs_to :user, foreign_key: 'user_ref', primary_key: 'name'
+  # looks up emails where lots.seller == user.name
+  belongs_to :user, foreign_key: 'seller', primary_key: 'name'
 end
 
 # Endpoints
+# Inspect setting 
 get '/' do
   "Port: #{settings.port}, Max lots: #{settings.max_lots}, Advertising fee: #{settings.advertising_fee}"
+end
+
+# Clean the database and create the initial data
+get '/refresh' do
+  load './seed.rb'
+  'Refresh success!'
 end
